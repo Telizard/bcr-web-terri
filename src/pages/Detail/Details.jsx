ece10cc4f0b6e9b9069a103110e65e17001eb0cc
telizard <terrysutansyah@gmail.com>
2023-05-21 21:35:32 +0700

    update 3

--- src/pages/Detail/Details.jsx
+++ src/pages/Detail/Details.jsx
@@ -145,7 +145,7 @@ const Detail = () => {
             <div className="card w-75 d-flex ">
               <div className="card-body justify-content-end ">
                 <div className=" pt-2 ">
-                  <img src={image} style={{ width: "300px", height: "200px" }} alt="" className="justify-content-center" />
+                  <img src={image} style={{ width: "300px", height: "150px" }} alt="" className="justify-content-center" />
                 </div>
                 <h6 className="text-start fw-bold pt-5">{name}</h6>
                 <div className="user1">
@@ -161,6 +161,33 @@ const Detail = () => {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="container text-start mt-5 pt-5">
+        <div className="row">
+          <div className="col">
+            <h6 className="fw-bold">Jalan Suroyo No. 161 Mayangan Kota Probolonggo 672000</h6>
+            <h6 className="fw-bold pt-3">binarcarrental@gmail.com</h6>
+            <h6 className="fw-bold pt-3">081-233-334-808</h6>
+          </div>
+          <div className="col services">
+            <h6 className>Our services</h6>
+            <h6 className="pt-3">Why Us</h6>
+            <h6 className="pt-3">Testimonial</h6>
+            <h6 className="pt-3">FAQ</h6>
+          </div>
+          <div className="col text-start ">
+            <h6 className="fw-bold">Connect with us</h6>
+            <img className="m-1 pt-2" src={require("../../components/aset/facebook.png")} alt="" />
+            <img className="m-1 pt-2" src={require("../../components/aset/ig.png")} alt="" />
+            <img className="m-1 pt-2" src={require("../../components/aset/twitter.png")} alt="" />
+            <img className="m-1 pt-2" src={require("../../components/aset/mail.png")} alt="" />
+            <img className="m-1 pt-2" src={require("../../components/aset/twitch.png")} alt="" />
+          </div>
+          <div className="col">
+            <h6 className="fw-bold">Copyright Binar 2022</h6>
+            <img className="mt-2 pt-2" src={require("../../components/aset/logo.png")} alt="" />
           </div>
         </div>
       </div>
